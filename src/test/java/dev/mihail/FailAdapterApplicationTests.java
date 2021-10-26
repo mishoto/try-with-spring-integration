1a62356b6758eafe4fe63bff86f7b248e5270559
Mishoto <mihail.dev.io@gmail.com>
2021-10-26 07:50:28 +0300

    Initial Commit

---
+++ src/test/java/dev/mihail/FailAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mihail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
